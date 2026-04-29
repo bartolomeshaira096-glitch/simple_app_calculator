6b1e94b330c2060c34c79abad219615c6a1650cb
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-04-29 18:11:54 +0800

    Create parent calculator class as foundation for inheritance and arithmetic processing

--- math_warrior_calculator.py
+++ math_warrior_calculator.py
@@ -1,2 +1,20 @@
 import tkinter as tk
 import math
+
+class CalculatorCore:
+    def add_numbers(self, first_number, second_number):
+        return first_number + second_number
+    
+    def subtract_numbers(self, first_number, second_number):
+        return first_number - second_number
+    
+    def multiply_numbers(self, first_number, second_number):
+        return first_number * second_number
+    
+    def modulus_numbers(self, first_number, second_number):
+        return first_number % second_number
+    
+    def divide_numbers(self, first_number, second_number):
+        if second_number == 0:
+            return "Error"
+        return first_number / second_number
