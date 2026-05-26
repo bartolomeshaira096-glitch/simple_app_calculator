aaa589ecfcce9b3fd8e4fe8c1ca7e831a0602de2
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-26 20:39:16 +0800

    Add scientific calculator module for trigonometric operations

---
+++ scientific_calculator.py
@@ -0,0 +1,11 @@
+import math
+
+class ScientificCalculator:
+    def sin_deg(self, x):
+        return math.sin(math.radians(x))
+
+    def cos_deg(self, x):
+        return math.cos(math.radians(x))
+
+    def tan_deg(self, x):
+        return math.tan(math.radians(x))
